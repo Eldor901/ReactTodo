0d0529e94e132c293d33d1e292d25df83f1b1a54
Eldor901 <eldor9012545@gmail.com>
2020-03-23 07:05:32 +0300

    Seperated into components

---
+++ src/components/TodoInput.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class TodoInput extends Component {
+    render() {
+        return (
+            <div>
+                <h2>hello from todo Input</h2>
+            </div>
+        );
+    }
+}
+
+export default TodoInput;
